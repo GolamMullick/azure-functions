7da36bf621025d5ca182e6cebddff604a2126cbd
Golam Mullick <fahadmullick89@gmail.com>
2025-06-14 09:53:01 +1000

    new_line

--- function_app.py
+++ function_app.py
@@ -7,7 +7,7 @@ app = func.FunctionApp()
 
 @app.route(route="HttpExample", auth_level=func.AuthLevel.ANONYMOUS)
 def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Python HTTP trigger function processed a request normally.')
 
     name = req.params.get('name')
     if not name:
